9aa6f7f47d35767c6fe9ef49fd047a2458a9c3cb
oleg <music.wave.form7@gmail.com>
2022-12-22 16:10:58 +0100

    (env): add uikit to next app

--- apps/web2/pages/index.tsx
+++ apps/web2/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { SuperButton } from 'uikit';
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 
@@ -18,6 +19,7 @@ export default function Home() {
         <div className={styles.description}>
           <p>
             Get started by editing aux_edit&nbsp;
+            <SuperButton/>
             <code className={styles.code}>pages/index.tsx</code>
           </p>
           <div>
